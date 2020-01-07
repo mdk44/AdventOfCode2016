0b3677862b6c812511f9de317119b2a09b23b4b4
mdk44 <matt.karam@gmail.com>
2020-01-06 19:49:14 -0600

    Day 2 complete!

--- Day 2/Day 2.py
+++ Day 2/Day 2.py
@@ -9,9 +9,6 @@ lines = text_file.read().split('\n')
 # 4 5 6
 # 7 8 9
 
-button = 5 # Starting button
-code = ''
-
 def move_buttons(line, button):
     for l in range(0, len(line)):
         if line[l] == 'U' and button > 3:
@@ -24,8 +21,46 @@ def move_buttons(line, button):
             button += 1
     return button
 
+def move_buttons_p2(line, button):
+    for l in range(0, len(line)):
+        if line[l] == 'U' and button not in ('1', '2', '4', '5', '9'):
+            buttons = ['A', '6', '2', 'D', 'B', '7', '3', '1', 'C', '8', '4']
+            for b in range(0, len(buttons)):
+                if button == buttons[b]:
+                    button = buttons[b + 1]
+                    break
+        elif line[l] == 'D' and button not in ('5', 'A', 'D', 'C', '9'):
+            buttons = ['2', '6', 'A', '1', '3', '7', 'B', 'D', '4', '8', 'C']
+            for b in range(0, len(buttons)):
+                if button == buttons[b]:
+                    button = buttons[b + 1]
+                    break
+        elif line[l] == 'L' and button not in ('1', '2', '5', 'A', 'D'):
+            buttons = ['4', '3', '2', '9', '8', '7', '6', '5', 'C', 'B', 'A']
+            for b in range(0, len(buttons)):
+                if button == buttons[b]:
+                    button = buttons[b + 1]
+                    break
+        elif line[l] == 'R' and button not in ('1', '4', '9', 'C', 'D'):
+            buttons = ['2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C']
+            for b in range(0, len(buttons)):
+                if button == buttons[b]:
+                    button = buttons[b + 1]
+                    break
+    return button
+
+# Part 1
+button = 5 # Starting button
+code = ''
 for line in lines:
     button = move_buttons(line, button)
     code += str(button)
+print("Part 1: " + str(code)) # Correct!
 
-print("Part 1: " + str(code))
+# Part 2
+button2 = '5' # Starting button
+code = ''
+for line in lines:
+    button2 = move_buttons_p2(line, button2)
+    code += str(button2)
+print("Part 2: " + str(code)) # Correct!
