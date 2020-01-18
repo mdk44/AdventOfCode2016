08f79d633dd0ca2828ade37524d21683c5a04d8d
mdk44 <matt.karam@gmail.com>
2020-01-18 06:56:37 -0600

    Tinkering Day 11

--- Day 11/Day 11.py
+++ Day 11/Day 11.py
@@ -1,4 +1,12 @@
-input_file = 'Day 11\\Input.csv'
-# input_file = 'Day 11\\Test.csv'
-text_file = open(input_file)
-lines = text_file.read().split('\n')
+# Test
+F4 = ['  ', '  ', '  ', '  ', '  ']
+F3 = ['  ', '  ', '  ', 'LG', '  ']
+F2 = ['  ', 'HG', '  ', '  ', '  ']
+F1 = ['E ', '  ', 'HM', '  ', 'LM']
+
+# Actual
+# F4 = ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ']
+# F3 = ['  ', '  ', '  ', '  ', '  ', '  ', 'TM', '  ', '  ', '  ', '  ']
+# F2 = ['  ', '  ', '  ', '  ', '  ', 'TG', '  ', 'RG', 'RM', 'CG', 'CM']
+# F1 = ['E ', 'SG', 'SM', 'PG', 'PM', '  ', '  ', '  ', '  ', '  ', '  ']
+
