05dbc2b5c520bf7c41f79eed8e82cfcafe3d01c0
mdk44 <matt.karam@gmail.com>
2020-01-25 12:35:55 -0600

    Day 11 complete!

--- Day 11/Day 11.py
+++ Day 11/Day 11.py
@@ -1,12 +1,22 @@
-# Test
-F4 = ['  ', '  ', '  ', '  ', '  ']
-F3 = ['  ', '  ', '  ', 'LG', '  ']
-F2 = ['  ', 'HG', '  ', '  ', '  ']
-F1 = ['E ', '  ', 'HM', '  ', 'LM']
+items_test = [2, 1, 1, 0] # I firmly believe that the test "answer" on the site is wrong.  It should be 9
+items_p1 = [4, 5, 1, 0]
+items_p2 = [8, 5, 1, 0]
 
-# Actual
-# F4 = ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ', '  ']
-# F3 = ['  ', '  ', '  ', '  ', '  ', '  ', 'TM', '  ', '  ', '  ', '  ']
-# F2 = ['  ', '  ', '  ', '  ', '  ', 'TG', '  ', 'RG', 'RM', 'CG', 'CM']
-# F1 = ['E ', 'SG', 'SM', 'PG', 'PM', '  ', '  ', '  ', '  ', '  ', '  ']
+inp1 = items_p1
+inp2 = items_p2
 
+def move_items(n):
+    move = 2 * (n - 1) - 1 # This is how many moves it takes to move an entire floor up one floor (had to google)
+    return n, move
+
+def top_floor(inp):
+    moves = 0
+    for i in range(1, 4):
+        n, move = move_items(inp[i - 1])
+        inp[i - 1] -= n
+        inp[i] += n
+        moves += move
+    return moves
+
+print("Part 1: " + str(top_floor(inp1))) # Correct
+print("Part 2: " + str(top_floor(inp2))) # Correct
