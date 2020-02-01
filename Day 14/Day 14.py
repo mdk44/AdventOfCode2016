4797d5d07e87cb26aec34f2131e3b0341fd109bc
mdk44 <matt.karam@gmail.com>
2020-01-31 22:06:35 -0600

    Day 14 p1 complete, need to optimize for p2

--- Day 14/Day 14.py
+++ Day 14/Day 14.py
@@ -1,7 +1,7 @@
 import hashlib
 
-# salt = 'abc' # Test result is correct: 22728
-salt = 'qzyelonm'
+salt = 'abc' # Test result is correct: 22728
+# salt = 'qzyelonm'
 
 def md5(salt, index):
     inp = salt + str(index) 
@@ -9,33 +9,50 @@ def md5(salt, index):
     hex_code = code.hexdigest()
     return hex_code
 
-def triplets(salt, index):
-    inp = md5(salt, index)
+def md5_2017(salt, index):
+    i = 0
+    inp = salt + str(index)
+    while i < 2016:
+        code = hashlib.md5(inp.encode())
+        inp = code.hexdigest()
+        i += 1
+    code = hashlib.md5(inp.encode())
+    hex_code = code.hexdigest()
+    return hex_code
+
+def triplets(salt, index, part):
+    if part == 1:
+        inp = md5(salt, index)
+    elif part == 2:
+        inp = md5_2017(salt, index)
     for i in range(2, len(inp)):
         if inp[i - 2] == inp[i - 1] and inp[i - 1] == inp[i]:
             new = str(inp[i]) + str(inp[i]) + str(inp[i]) + str(inp[i]) + str(inp[i])
             return new
     return ''
 
-def check_index(salt, index):
-    new = triplets(salt, index)
+def check_index(salt, index, part):
+    new = triplets(salt, index, part)
     if len(new) == 0:
         return 0
     for i in range(index + 1, 1001 + index):
-        inp = md5(salt, i)
+        if part == 1:
+            inp = md5(salt, i)
+        elif part == 2:
+            inp = md5_2017(salt, i)
         if new in inp:
             return 1
     return 0
 
-def find_64(salt):
+def find_64(salt, part):
     key = 0
     index = 0
     p1 = False
     while p1 == False:
-        key += check_index(salt, index)
+        key += check_index(salt, index, part)
         if key == 64:
             p1 = True
             return index
         index += 1
 
-print("Part 1: " + str(find_64(salt))) # Correct!
+print(find_64(salt, 1)) # Correct!
